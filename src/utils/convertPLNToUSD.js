ccb47725be3426122378fed28cf87f8ac21e6003
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-02-04 20:02:08 +0100

    Add support for convertPLNToUSD: tests and change function

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,27 @@
+export const convertPLNToUSD = (PLN) => {
+
+  if (PLN === undefined) {
+    return NaN;
+  }
+
+  if (typeof PLN === 'string') {
+    return NaN;
+  }
+
+  if (typeof PLN !== 'number' && typeof PLN !== 'string') {
+    return "Error";
+  }
+
+  if (PLN < 0) {
+    return '$0.00';
+  }
+
+  const PLNtoUSD = PLN / 3.5;
+  
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
